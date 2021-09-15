efc04a2436393539c8c05f3928b2042b3edf16fd
Kyle-Wi <black1848@naver.com>
2021-09-15 16:31:06 +0900

    20210915 chatapp start

---
+++ src/main/java/com/chatapp/chatapps/ChatappsApplication.java
@@ -0,0 +1,13 @@
+package com.chatapp.chatapps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatappsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatappsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chatapp/chatapps/ChatappsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chatapp.chatapps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatappsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
